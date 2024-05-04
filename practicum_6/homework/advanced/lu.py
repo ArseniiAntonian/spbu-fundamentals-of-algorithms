3fe4961d72c8673994801ff560ce73d1129362e3
Arsenii Antonian <zvnlxn@gmail.com>
2024-05-04 09:41:25 +0300

    add hw6

--- practicum_6/homework/advanced/lu.py
+++ practicum_6/homework/advanced/lu.py
@@ -9,8 +9,11 @@ from numpy.typing import NDArray
 import scipy.io
 import scipy.linalg
 
-from src.linalg import get_scipy_solution
+#from src.linalg import get_scipy_solution
 
+def get_scipy_solution(A, b):
+    lu_and_piv = scipy.linalg.lu_factor(A)
+    return scipy.linalg.lu_solve(lu_and_piv, b)
 
 @dataclass
 class Performance:
@@ -19,21 +22,46 @@ class Performance:
 
 
 def lu(A: NDArray, permute: bool) -> tuple[NDArray, NDArray, NDArray]:
+    n = len(A)
+    U = np.copy(A)
+    L = np.eye(n)
+    P = np.eye(n)
+    for k in range(n - 1):
+        if permute:
+            pivot_index = np.argmax(np.abs(U[k:, k])) + k
+            if pivot_index != k:
+                U[[k, pivot_index]] = U[[pivot_index, k]]
+                P[[k, pivot_index]] = P[[pivot_index, k]]
+                if k > 0:
+                    L[[k, pivot_index], :k] = L[[pivot_index, k], :k]
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+            L[k+1:, k] = U[k+1:, k] / U[k, k]
+            U[k+1:, k:] -= np.outer(L[k+1:, k], U[k, k:])
+        else:
+            U_0 = np.eye(n)
+            for i in range(k + 1, n):
+                m = U[i, k] / U[k, k]
+                U_0[i, k] = -m
+                L[i, k] = m
+            U = U_0 @ U
 
-    pass
+    return L, U, P
 
 
 def solve(L: NDArray, U: NDArray, P: NDArray, b: NDArray) -> NDArray:
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
-
-    pass
+    b = P @ b
+    
+    n = L.shape[0]
+    y = np.zeros(n)
+    for i in range(n):
+        y[i] = b[i] - np.dot(L[i, :i], y[:i])
+    
+    x = np.zeros(n)
+    for i in range(n - 1, -1, -1):
+        x[i] = (y[i] - np.dot(U[i, i+1:], x[i+1:])) / U[i, i]
+    
+    return x
 
 
 def run_test_cases(n_runs: int, path_to_homework: str) -> dict[str, Performance]:
@@ -55,7 +83,7 @@ def run_test_cases(n_runs: int, path_to_homework: str) -> dict[str, Performance]
             L, U, P = lu(A, permute=True)
             t2 = time.time()
             perf.time += t2 - t1
-            if j == 0:  # first run => compute solution
+            if j == 0:  
                 x = solve(L, U, P, b)
                 x_exact = get_scipy_solution(A, b)
                 perf.relative_error = np.linalg.norm(x - x_exact) / np.linalg.norm(
